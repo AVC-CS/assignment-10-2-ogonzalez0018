f31b3237938b0ed4a48963a7738c05f44fc7a74d
Kyuwoong Lee <kwooler@gmail.com>
2024-01-30 16:05:27 -0800

    Initial commit

---
+++ main.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+
+const int MAXNUM = 10;
+const int MAXLEN = 100;
+
+int splitline(string, char, char[][MAXLEN]);
+
+/*******************************
+ * Code your program here
+ *******************************/
