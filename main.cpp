daab348e5582d738d98100aacf77d9fef35afcb2
ogonzalez0018 <156852162+ogonzalez0018@users.noreply.github.com>
2024-02-20 18:46:23 +0000

    102

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
 #include "main.hpp"
 
-int main()
-{
-	char people[M][N][L];
-	int length;
-	int num;
+int main() {
+  char people[M][N][L];
+  int length;
+  int num;
 
-	length = makeDB(people);
-	cout << "The number of record is " << length << endl;
-	printRecord(people, length);
-	printEmail(people, length);
-	num = getNumMale(people, length);
-	cout << "The number of Male Employee: " << num << endl;
+  length = makeDB(people);
+  cout << "The number of record is " << length << endl;
+  printRecord(people, length);
+  printEmail(people, length);
+  num = getNumMale(people, length);
+  cout << "The number of Male Employee: " << num << endl;
 }
--- main.hpp
+++ main.hpp
@@ -14,10 +14,40 @@ int printTel(char[], int);
 
 int makeDB(char people[][N][L])
 {
+  int length = 0;
+  char line[256];
+  ifstream ifs;
+  ifs.open("people-100.csv");
+  if(!ifs)
+  {
+    cerr << "Error opening file" << endl;
+    exit(0);
+  }
+  while(ifs.getline(line, 256))
+    {
+      int i = 0, j = 0;
+      int field = 0;
+      // cout << line << endl;
+      while(line[i] != '\0')
+        {
+          if(line[i] != ',')
+            people[length][field][j++] = line[i];
+          else
+          {
+            people[length][field][j] = '\0'; 
+            // cout << people[length][field] << "\t";
+            field++;
+            j = 0;
+          }
+          i++;
+        }
+      length++;
+    }
     return length;
 }
 void printRecord(char people[][N][L], int length)
 {
+  printf("NO\tID\t\tName\t\t\tEmail\t\tPhone\t\t\tBirthday\t\t\t\tJobtitle\n");
     for (int i = 0; i < length; i++)
     {
         for (int j = 0; j < N; j++)
@@ -29,9 +59,19 @@ void printRecord(char people[][N][L], int length)
 }
 void printEmail(char people[][N][L], int length)
 {
+    for (int i = 0; i < length; i++)
+    {
+      cout << people[i][5] << endl;
+    }
 }
 int getNumMale(char people[][N][L], int length)
 {
+  int cnt = 0;
+  for (int i = 0; i < length; i++)
+    if (strcmp("Male", people[i][4]) == 0)
+      cnt++;
+
+  return cnt;
 }
 
 /*******************************
